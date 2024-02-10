48c0ad661f3b9fff3338832276ca895c07002545
KatiaAydin <60203441+Goomisica@users.noreply.github.com>
2024-02-10 14:06:53 -0800

    aa computer bad

---
+++ src/devicesManager.hpp
@@ -0,0 +1,67 @@
+#ifndef DEVICESMANAGER_HPP
+#define DEVICESMANAGER_HPP
+
+// BT includes
+#define WIN32_LEAN_AND_MEAN 1 // Don't include Winsock v1
+#include <windows.h>
+#include <stdlib.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+#include <bluetoothapis.h>
+#include <WinSock2.h>
+#include <ws2bth.h>
+
+// Link with Bluetooth lib
+#pragma comment(lib, "Bthprops.lib")
+
+// normal includes
+#include <vector>
+#include <iostream>
+
+class DevicesManager {
+    private:
+        std::vector<BLUETOOTH_DEVICE_INFO> devices;
+    public:
+        /**
+         * @return SOCKET ERORR: -1
+         * @return SUCCESS: 0
+        */
+        int initCurrentlyRemembered() {
+            BLUETOOTH_DEVICE_SEARCH_PARAMS searchCriteria;
+            // init all necessary values for search (only return paired devices)
+                searchCriteria.dwSize = sizeof(BLUETOOTH_DEVICE_SEARCH_PARAMS);
+                searchCriteria.fReturnAuthenticated = false;
+                searchCriteria.fReturnRemembered = true;
+                searchCriteria.fReturnUnknown = false;
+                searchCriteria.fReturnConnected = false;
+                searchCriteria.fIssueInquiry = false;
+                // timeout- infinite time prob.
+                searchCriteria.cTimeoutMultiplier = 0;
+
+            // set up device struct
+            BLUETOOTH_DEVICE_INFO deviceInfo;
+                deviceInfo.dwSize = sizeof(BLUETOOTH_DEVICE_INFO);
+            
+            // get first device (to check if there are any to pair)
+            HBLUETOOTH_DEVICE_FIND foundDevice = BluetoothFindFirstDevice(&searchCriteria, &deviceInfo);
+            if (!foundDevice) {
+                return SOCKET_ERROR;
+            }
+
+            // loop through all valid devices and add them to internal vector
+            do {
+                this->devices.push_back(deviceInfo);
+            } while (BluetoothFindNextDevice(foundDevice, &deviceInfo));
+        }
+        void printDevices() {
+            std::cout << "currently paried:" <<  std::endl;
+            for (int i = 0; i < this->devices.size(); i++) {
+                BLUETOOTH_DEVICE_INFO info = this->devices.at(i);
+                std::cout << info.szName << std::endl;
+            }
+        }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
-#define WIN32_LEAN_AND_MEAN 1 // Don't include Winsock v1
-#include <windows.h>
-#include <stdlib.h>
-#pragma comment(lib, "ws2_32.lib")
-
-#include <ws2bth.h>
-#include <WS2tcpip.h>
-
-#include <iostream>
+#include "devicesManager.hpp"
 
 int main() {
+    DevicesManager manager;
+    manager.initCurrentlyRemembered();
+    manager.printDevices();
+    
     int a;
     std::cin >> a;
     return 0;
